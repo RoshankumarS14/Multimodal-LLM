bd72356a0adf94d5c27804227070d78e963a1578
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2025-02-17 05:52:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
     if 'OPENAI_API_KEY' in os.environ:
         st.success('API key already provided!', icon='✅')
-        openai.api_key = os.getenviron['OPENAI_API_KEY']
+        openai.api_key = os.getenv['OPENAI_API_KEY']
     else:
         openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
         if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
